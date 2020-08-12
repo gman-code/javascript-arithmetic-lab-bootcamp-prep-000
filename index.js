e51323802e5e4bf88e5ccd6580b00ebd9ea59682
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-12 22:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function add(a, b){
 function subtract(a, b){
   return a-b 
 }
-function multiply (a, b){
+function multiple (a, b){
   return a* b 
 }
 function divide(a, b){
   return a/b
 }
 
-
+multiply (6*6)
 
 
